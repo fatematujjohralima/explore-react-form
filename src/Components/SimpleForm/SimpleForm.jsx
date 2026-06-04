10d90826d18e99798cf775a283b76827758e0514
fatematujjohra <fatematujjohralima2@gmail.com>
2026-06-04 21:34:12 +0600

    Access form data using onSubmit and event target

--- src/Components/SimpleForm/SimpleForm.jsx
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -5,12 +5,19 @@ const SimpleForm = () => {
     const handleSubmit =(e) => {
         e.preventDefault();
         alert('Form submitted');
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        console.log(e.target.password.value);
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Enter your name" />
+                <input type="text" name='name' placeholder="Enter your name" />
+                <br />
+                <input type="email" name='email' placeholder="Enter your email" />
+                <br />
+                <input type="password" name='password' placeholder="Enter your password" />
                 <br />
                 <input type="submit" value="Submit" />
             </form>
