bcf890fd1e6a895dff3b54e4f917de6fd89982db
fatematujjohra <fatematujjohralima2@gmail.com>
2026-06-07 20:45:42 +0600

    Add ProductForm and ProductTable components for product management

---
+++ src/Components/ProductManagement/ProductManagement.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ProductTable from './ProductTable';
+import ProductForm from './ProductForm';
+
+const ProductManagement = () => {
+    return (
+        <div>
+            <ProductForm></ProductForm>
+            <ProductTable></ProductTable>
+            
+        </div>
+    );
+};
+
+export default ProductManagement;
